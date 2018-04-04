dd7617c0fe5ac0bdec5e8b14c1dd4d89ac224f81
Rob Horton <robhorton@outlook.com>
2018-04-04 10:41:54 -0500

    Creating StringExtensions.cs

---
+++ ObservaFile5000/StringExtensions.cs
@@ -0,0 +1,39 @@
+
+using System.Text.RegularExpressions;
+
+namespace ObservaFile5000
+{
+    public static class StringExtensions
+    {
+        public static string TrimAndReduce(this string str)
+        {
+            return ConvertWhitespacesToSingleSpaces(str).Trim();
+        }
+
+        public static string ConvertWhitespacesToSingleSpaces(this string value)
+        {
+            return Regex.Replace(value, @"\s+", " ");
+        }
+
+        public static string FormatAsPhoneNumber(this string value) {
+            if (value == null || value.Length < 10) return "(###) ###-####";
+            var converted = ConvertToNumbersOnly(value);
+            var totalLength = converted.Length;
+
+            string phone = string.Empty, prefix = string.Empty;
+
+            if (totalLength > 10) {
+                phone = converted.Substring(totalLength - 10, 10);
+                prefix = $"+{converted.Substring(0, totalLength - phone.Length)} ";  
+            }
+
+            var result = $"{prefix}({converted.Substring(0, 3)}) {converted.Substring(3, 3)}-{converted.Substring(6, 4)}";
+            return result;
+        }
+
+        public static string ConvertToNumbersOnly(this string value) {
+            if (value == null) return string.Empty;
+            return Regex.Replace(value, @"[^\d]", "");
+        }
+    }
+}
